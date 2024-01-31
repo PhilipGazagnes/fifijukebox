3e784be7609ec963f2273bd28d40e580972dee21
Philip Gazagnes <p.gazagnes@vacanceselect.com>
2024-01-31 13:58:20 +0100

    init

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///opt/projects_perso/fifijukebox/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import indexJson from '../data/index.json'
+
+type Song = any
+
+const songs: Song[] = indexJson
+</script>
+
+<template>
+  <div>
+    <h1>Fifi Juke Box</h1>
+    <ul>
+      <li v-for="(song, index) in songs" :key="index">
+        <a :href="`/songs/${song.id}`">{{ song.name }} ({{ song.artist }})</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+body, html {
+  margin: 0;
+  padding: 0;
+}
+</style>
