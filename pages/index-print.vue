9e54f5071eb8d04d4e0adf69bc90c544727193e5
Philip Gazagnes <p.gazagnes@vacanceselect.com>
2024-03-22 16:15:47 +0100

    index print & index update

---
+++ pages/index-print.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import indexJson from '../data/indexByArtist.json'
+
+type Song = any
+
+const artists: Song[] = indexJson
+</script>
+
+<template>
+  <ul>
+    <li v-for="(artist, index) in artists" :key="index">
+      <h4 class="font-bold">{{ artist.artist }}</h4>
+      <ul>
+        <li v-for="(song, index2) in artist.items" :key="index2">
+          {{ song.name }}
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
